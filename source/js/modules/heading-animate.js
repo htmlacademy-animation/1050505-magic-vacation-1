4b221b11b6e0c60874b00f23cf2b2ad5e39882bf
Olga Garder <o.garder@blanc.ru>
2023-04-03 12:39:36 +0300

    add animate canvas crocodile

--- source/js/modules/heading-animate.js
+++ source/js/modules/heading-animate.js
@@ -1,4 +1,5 @@
-import WinCanvasAnimation from './canvas-animate.js';
+import WinCanvasAnimation from './canvas-animate';
+import CrocodileAnimation from './crocodile-canvas';
 
 export default () => {
   document.querySelectorAll(`.game__button`).forEach((button) => {
@@ -6,6 +7,9 @@ export default () => {
       if (button.dataset.target === `result`) {
         const winCanvasAnim = new WinCanvasAnimation();
         winCanvasAnim.start();
+      } else if (button.dataset.target === `result3`) {
+        const crocodileAnim = new CrocodileAnimation();
+        crocodileAnim.start();
       }
     });
   });

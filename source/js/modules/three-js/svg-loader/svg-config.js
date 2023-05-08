fcf52878272bbfe29ee62c9d6b67814137be79d4
Olga Garder <o.garder@blanc.ru>
2023-05-08 13:59:29 +0300

    6.34. Добавление однотонных материалов личного проекта для ранее созданных объектов

--- source/js/modules/three-js/svg-loader/svg-config.js
+++ source/js/modules/three-js/svg-loader/svg-config.js
@@ -1,47 +1,56 @@
+import {colors} from "../scenes/helpers/colors";
+import {reflectivity} from "../scenes/helpers/reflectivity";
+
 const svgConfig = {
-    keyHole: {
-      src: `./img/module-6/svg-forms/keyhole.svg`,
-      color: `#4e4069`,
-      depth: 20,
-      cap: 0
-    },
-    leaf: {
-      src: `./img/module-6/svg-forms/leaf.svg`,
-      color: `#34df96`,
-      depth: 8,
-      cap: 2
-    },
-    leafPyramid: {
-      src: `./img/module-6/svg-forms/leaf.svg`,
-      color: `#20c14f`,
-      depth: 3,
-      cap: 3
-    },
-    flamingo: {
-      src: `./img/module-6/svg-forms/flamingo.svg`,
-      color: `#fe6183`,
-      depth: 8,
-      cap: 2
-    },
-    question: {
-      src: `./img/module-6/svg-forms/question.svg`,
-      color: `#3b7bf2`,
-      depth: 8,
-      cap: 2
-    },
-    snowflake: {
-      src: `./img/module-6/svg-forms/snowflake.svg`,
-      color: `#3b7bf2`,
-      depth: 8,
-      cap: 2
-    },
-    flower: {
-      src: `./img/module-6/svg-forms/flower.svg`,
-      color: `#664ba5`,
-      depth: 4,
-      cap: 2
-    }
-  };
-  
-  export default svgConfig;
-  
+  keyHole: {
+    src: `./img/module-6/svg-forms/keyhole.svg`,
+    color: colors.DarkPurple,
+    depth: 20,
+    cap: 0,
+    reflectivity: reflectivity.soft
+  },
+  leaf: {
+    src: `./img/module-6/svg-forms/leaf.svg`,
+    color: colors.Green,
+    depth: 8,
+    cap: 2,
+    reflectivity: reflectivity.basic
+  },
+  leafPyramid: {
+    src: `./img/module-6/svg-forms/leaf.svg`,
+    color: colors.Green,
+    depth: 3,
+    cap: 3,
+    reflectivity: reflectivity.basic
+  },
+  flamingo: {
+    src: `./img/module-6/svg-forms/flamingo.svg`,
+    color: colors.LightDominantRed,
+    depth: 8,
+    cap: 2,
+    reflectivity: reflectivity.soft
+  },
+  question: {
+    src: `./img/module-6/svg-forms/question.svg`,
+    color: colors.Blue,
+    depth: 8,
+    cap: 2,
+    reflectivity: reflectivity.basic
+  },
+  snowflake: {
+    src: `./img/module-6/svg-forms/snowflake.svg`,
+    color: colors.Blue,
+    depth: 8,
+    cap: 2,
+    reflectivity: reflectivity.basic
+  },
+  flower: {
+    src: `./img/module-6/svg-forms/flower.svg`,
+    color: colors.ShadowedPurple,
+    depth: 4,
+    cap: 2,
+    reflectivity: reflectivity.basic
+  }
+};
+
+export default svgConfig;
